316e23a192fbee70cc4324ca7fee8ddff644f446
Jeffrey Yasskin <jyasskin@google.com>
2023-10-26 15:01:52 -0700

    Simplify the Instant zod type.

--- frontend/src/lib/repo-summaries.ts
+++ frontend/src/lib/repo-summaries.ts
@@ -1,15 +1,16 @@
-import { Temporal, toTemporalInstant } from '@js-temporal/polyfill';
+import { Temporal } from '@js-temporal/polyfill';
 import { z } from 'zod';
 
 export const SloType = z.enum(["triage", "urgent", "important", "none"]);
 export type SloType = z.infer<typeof SloType>;
 
 const duration = z.string().transform(val => Temporal.Duration.from(val));
+const instant = z.string().transform(val => Temporal.Instant.from(val));
 
 export const IssueSummary = z.object({
     url: z.string().url(),
     title: z.string(),
-    createdAt: z.coerce.date().transform(val => toTemporalInstant.call(val)),
+    createdAt: instant,
     pull_request: z.object({ draft: z.boolean().default(false) }).optional(),
     labels: z.array(z.string()),
     sloTimeUsed: duration,
@@ -23,7 +24,7 @@ export const IssueSummary = z.object({
 export type IssueSummary = z.infer<typeof IssueSummary>;
 
 export const RepoSummary = z.object({
-    cachedAt: z.string().transform(val => Temporal.Instant.from(val)),
+    cachedAt: instant,
     org: z.string(),
     repo: z.string(),
     issues: IssueSummary.array(),
