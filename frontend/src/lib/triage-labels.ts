2e7fdafca80f3bab0403403c725353f58c1bb6f8
Jeffrey Yasskin <jyasskin@google.com>
2023-10-26 22:04:14 -0700

    Explain what's going on, on the front page.

---
+++ frontend/src/lib/triage-labels.ts
@@ -0,0 +1,4 @@
+export const urgent = { name: "Priority: Urgent", color: "#b60205" } as const;
+export const important = { name: "Priority: Important", color: "#fbca04" } as const;
+export const eventually = { name: "Priority: Eventually", color: "#c2e0c6" } as const;
+export const needsFeedback = { name: "Needs Reporter Feedback", color: "#5319e7" } as const;
